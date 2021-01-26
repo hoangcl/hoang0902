21a5586a745758d94f52af1915d90fc221a62ae6
hoangcl <77031591+hoangcl@users.noreply.github.com>
2021-01-26 13:23:35 +0700

    Add files via upload

---
+++ BàiTâpChương5/src/Baitap5_4.java
@@ -0,0 +1,16 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Baitap5_4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+
+            }
+
+        }
+    }
+}
