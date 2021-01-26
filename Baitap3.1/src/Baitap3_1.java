a5fda84a5e68633f2c14b2efca3324a216a03204
hoangcl <77031591+hoangcl@users.noreply.github.com>
2021-01-26 13:11:07 +0700

    Add files via upload

---
+++ Baitap3.1/src/Baitap3_1.java
@@ -0,0 +1,9 @@
+public class Baitap3_1 {
+      //Khai bao doi tuong input cua lop Scanner
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
