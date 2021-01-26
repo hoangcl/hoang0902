e388ad04ed99361d7ad5c8a0482ede9235f783b9
hoangcl <77031591+hoangcl@users.noreply.github.com>
2021-01-26 13:20:42 +0700

    Add files via upload

---
+++ Baitapjavacobanvoimang/src/Baitap4.java
@@ -0,0 +1,10 @@
+public class Baitap4 {
+    public static void main(String[] args) {
+        int arr[][]={{5,12,17,9,3},{13,4,8,6,1},{9,6,3,7,21}};
+        for(int i=2;i<3;i++){
+            for(int j=0;j<5;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+        }
+    }
+}
